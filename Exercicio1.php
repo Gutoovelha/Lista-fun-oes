09e7c8d80c8d453a4ff665983ac8d78c6e2ae31e
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-06-14 14:54:38 -0300

    Create Exercicio1.php

---
+++ Exercicio1.php
@@ -0,0 +1,13 @@
+<?php
+
+    function graus ($temp){
+        $celsius = ($temp - 32 ) * (5/9);
+        $msg = "a temperatura $temp" . "º" . " está em fahrenheit, convertendo para celsius, é igual a: $celsius" . "º celsius\n";
+        return $msg;
+    }
+    
+    echo "Digite a temperatura em fahrenheit: \n";
+    $temp = (int) fgets (STDIN);
+    
+    $status = graus ($temp);
+    echo $status;
